829b64a9456d4ed625b4042c5babd2437fddaf87
bshin132 <bshin132@gmail.com>
2022-01-21 06:30:48 +0000

    complete iss spotter2

---
+++ index.js
@@ -0,0 +1,21 @@
+const { fetchMyIP } = require('./iss');
+
+fetchMyIP((error, ip) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned IP:' , ip);
+});
+
+const { fetchCoordsByIP } = require('./iss');
+
+fetchCoordsByIP('162.245.144.188', (error, coordinates) => {
+  if (error) {
+    console.log("It didn't work!" , error);
+    return;
+  }
+
+  console.log('It worked! Returned coordinates:' , coordinates);
+});
